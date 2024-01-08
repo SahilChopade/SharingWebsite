5bb10b0acba3db24aa49f26d9750d3881ac30805
SahilChopade <chopade.sahil02@gmail.com>
2024-01-08 10:31:53 +0530

    added path

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { MulterError } = require("multer");
 const multer = require("multer");
 const bcrypt = require("bcrypt");
+const path = require('path');
 const upload = multer({ dest: path.join(__dirname, 'tmp') });
 const File = require("./models/File");
 const nodemailer = require("nodemailer");
